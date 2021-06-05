6607504f7a37f2016f1310d1babade777f828269
sebastianstoelen <sebastian.stoelen@gmail.com>
2021-06-05 15:47:27 +0200

    Homepage WIP

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-  <Hello></Hello>
+  <div class="container mx-auto h-screen">
+    <Hello></Hello>
+    <h3 class="font-header text-primary text-4xl font-black">Projects</h3>
+    <ProjectSummary
+      title="Test title"
+      description="Some test description which will be a bit longer that the title itself"
+    ></ProjectSummary>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Hello from "./components/Hello.vue";
+import ProjectSummary from "./components/ProjectSummary.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     Hello,
-  }
+    ProjectSummary,
+  },
 });
 </script>
 
---
+++ src/components/ProjectSummary.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid grid-cols-2 content-center gap-8 p-8">
+    <img class="w-full rounded-3xl" src="/src/assets/images/BuurtBox.png" />
+    <div>
+      <h3 class="font-header text-primary text-4xl font-black">{{ title }}</h3>
+      <p class="font-body">{{ description }}</p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProjectSummary",
+  props: {
+    title: String,
+    description: String,
+  },
+});
+</script>
