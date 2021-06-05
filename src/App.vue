015cc85e377786654780381d9738cbfee2f32350
sebastianstoelen <sebastian.stoelen@gmail.com>
2021-06-05 16:38:54 +0200

    Project Summary WIP

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="container mx-auto h-screen">
-    <Hello></Hello>
+  <div class="container mx-auto h-screen p-16">
+    <Hello class="h-screen"></Hello>
     <h3 class="font-header text-primary text-4xl font-black">Projects</h3>
-    <ProjectSummary
-      title="Test title"
-      description="Some test description which will be a bit longer that the title itself"
-    ></ProjectSummary>
+    <div class="space-y-16 pt-16">
+      <ProjectSummary
+        imageSrc="/src/assets/images/BuurtBox.png"
+        title="BuurtBox: Ecological food made easy"
+        :description="buurtBoxDescription"
+      ></ProjectSummary>
+      <ProjectSummary
+        imageSrc="/src/assets/images/Chillzz.png"
+        title="Chillzz: Motivating a healthy work-life balance"
+        :description="chillzDescription"
+      ></ProjectSummary>
+    </div>
   </div>
 </template>
 
@@ -19,6 +27,14 @@ export default defineComponent({
   components: {
     Hello,
     ProjectSummary,
+  },
+  data: function() {
+    return {
+      buurtBoxDescription:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et nisl laoreet purus, amet, dolor id. Diam imperdiet leo tincidunt mi. Imperdiet velit vivamus interdum pharetra ultricies nulla. Sed placerat amet, eu et. Justo sodales sagittis placerat urna. Pulvinar aenean etiam enim, sed et. Neque sollicitudin velit integer interdum ut maecenas.",
+      chillzDescription:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et nisl laoreet purus, amet, dolor id. Diam imperdiet leo tincidunt mi. Imperdiet velit vivamus interdum pharetra ultricies nulla. Sed placerat amet, eu et. Justo sodales sagittis placerat urna. Pulvinar aenean etiam enim, sed et. Neque sollicitudin velit integer interdum ut maecenas.",
+    };
   },
 });
 </script>
--- src/components/ProjectSummary.vue
+++ src/components/ProjectSummary.vue
@@ -1,9 +1,26 @@
 <template>
-  <div class="grid grid-cols-2 content-center gap-8 p-8">
-    <img class="w-full rounded-3xl" src="/src/assets/images/BuurtBox.png" />
-    <div>
+  <div class="grid grid-cols-2 align-middle gap-8">
+    <img
+      class="
+        col-auto
+        w-full
+        h-96
+        object-cover
+        rounded-3xl
+        border-4 border-black
+        transition
+        duration-500
+        ease-in-out
+        transform
+        hover:-translate-y-1
+        hover:shadow-2xl
+        hover:scale-110
+      "
+      :src="imageSrc"
+    />
+    <div class="col-auto flex flex-col justify-center">
       <h3 class="font-header text-primary text-4xl font-black">{{ title }}</h3>
-      <p class="font-body">{{ description }}</p>
+      <p class="font-body mt-4">{{ description }}</p>
     </div>
   </div>
 </template>
@@ -13,6 +30,7 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "ProjectSummary",
   props: {
+    imageSrc: String,
     title: String,
     description: String,
   },
