7ee176a3f096d752846e37ddd5c1d3432f8eab0f
sebastianstoelen <sebastian.stoelen@gmail.com>
2021-06-19 13:55:12 +0200

    Import images properly

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
     <h3 class="font-header text-primary text-4xl font-black">Projects</h3>
     <div class="space-y-16 py-16">
       <ProjectSummary
-        imageSrc="/src/assets/images/BuurtBox.jpg"
+        :imageSrc="buurtBoxImage"
         title="BuurtBox: Ecological food made easy"
         :description="buurtBoxDescription"
       ></ProjectSummary>
       <ProjectSummary
-        imageSrc="/src/assets/images/Chillz.jpg"
+        :imageSrc="chillzImage"
         title="Chillzz: Motivating a healthy work-life balance"
         :description="chillzDescription"
       ></ProjectSummary>
@@ -21,6 +21,8 @@
 import { defineComponent } from "vue";
 import Hello from "./components/Hello.vue";
 import ProjectSummary from "./components/ProjectSummary.vue";
+import chillzImage from './assets/images/Chillz.jpg';
+import buurtBoxImage from './assets/images/BuurtBox.jpg';
 
 export default defineComponent({
   name: "App",
@@ -33,7 +35,10 @@ export default defineComponent({
       buurtBoxDescription:
         "BuurtBox is a platform stimulating a more sustainable shopping habit in consumers shopping for food. BuurtBox aims to provide consumers a more user-friendly and online shopping experience, while still stimulating a more sustainable shopping habit by connecting local dealers of sustainale foord to local shoppers. BuurtBox is making the easy choice the sustainable choice.",
       chillzDescription:
-        "Now the corona pandemic is slowly subsiding (at least in Belgium, knock on wood 🤞) we’re moving into a more hybrid way of working. Research suggest working from home makes it harder for people to make a clear distinction between their work an private life, potentially having a negative effect on their work balance. It’s even shown that working from home can increase burnouts. Chillz aims to encourage workers to take some time off during the day by encouraging planning of personal time and providing incentives to take up this personal time."
+        "Now the corona pandemic is slowly subsiding (at least in Belgium, knock on wood 🤞) we’re moving into a more hybrid way of working. Research suggest working from home makes it harder for people to make a clear distinction between their work an private life, potentially having a negative effect on their work balance. It’s even shown that working from home can increase burnouts. Chillz aims to encourage workers to take some time off during the day by encouraging planning of personal time and providing incentives to take up this personal time.",
+      chillzImage: chillzImage,
+      buurtBoxImage: buurtBoxImage
+
     };
   },
 });
