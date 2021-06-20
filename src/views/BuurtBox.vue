90a1c5abc79bafafd260adccdd44cc3d3301e34c
sebastianstoelen <sebastian.stoelen@gmail.com>
2021-06-20 15:45:17 +0200

    add tagline

--- src/views/BuurtBox.vue
+++ src/views/BuurtBox.vue
@@ -1,34 +1,53 @@
 <template>
-    <Header></Header>
-    <img
-      class="
-        w-full
-        h-full
-        object-cover
-        rounded-3xl
-        border-4 border-black
-      "
-      :src="buurtBoxWide"
-    />
-    
+  <Header></Header>
+  <img
+    class="w-full h-full object-cover rounded-3xl border-4 border-black"
+    :src="buurtBoxWide"
+  />
+  <div class="p-8">
+    <div class="flex w-full justify-center space-x-8">
+      <div class="flex space-x-2">
+        <CalendarIcon class="h-5 w-5"></CalendarIcon>
+        <p class="font-body">October 2020 - May 2021</p>
+      </div>
+      <div class="flex space-x-2">
+        <UserIcon class="h-5 w-5"></UserIcon>
+        <p class="font-body">UX Researcher, Designer</p>
+      </div>
+      <div class="flex space-x-2">
+        <TerminalIcon class="h-5 w-5"></TerminalIcon>
+        <p class="font-body">Figma, Miro, SquareSpace</p>
+      </div>
+    </div>
+    <div class="flex justify-center pt-16">
+      <h1 class="w-2/3 font-header text-5xl font-black text-center">
+        A platform connecting consumers with local shopkeepers selling
+        sustainable foods.
+      </h1>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import UnderConstruction from "@/components/errors/UnderConstruction.vue"
+import UnderConstruction from "@/components/errors/UnderConstruction.vue";
 import Header from "@/components/Header.vue";
 import buurtBoxWide from "@/assets/images/BuurtBox-wide.jpg";
+import { UserIcon, CalendarIcon, TerminalIcon } from "@heroicons/vue/outline";
 
 export default defineComponent({
-    name: "BuurtBox",
-    components: {
-        UnderConstruction,
-        Header
-    },
-    data: function () {
-        return {
-            buurtBoxWide: buurtBoxWide
-        }
-    }
-})
+  name: "BuurtBox",
+  components: {
+    UnderConstruction,
+    Header,
+    UserIcon,
+    CalendarIcon,
+    TerminalIcon,
+  },
+  data: function () {
+    return {
+      buurtBoxWide: buurtBoxWide,
+    };
+  },
+});
 </script>
