72159ed98d209af01998819891a05fa575bfa34f
sebastianstoelen <sebastian.stoelen@gmail.com>
2021-06-20 15:05:09 +0200

    create header

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="fixed w-screen top-0 left-0 bg-background z-50">
+    <div class="flex items-center px-8 py-4">
+      <img
+        class="w-16 object-cover m-1 mr-4 cursor-pointer"
+        :src="avatar"
+        @click="navigateHome"
+      />
+      <div class="-mt-2">
+        <h3 class="text-primary font-header text-3xl font-black">
+          Sebastian Stoelen
+        </h3>
+        <p class="font-body text-xs">
+          UX Designer by heart ❤️ | Coder by trade 💼 | Deep sleeper by night 😴
+        </p>
+      </div>
+    </div>
+    <div class="bg-primary h-0.5 w-full"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import avatar from "@/assets/images/avatar.png";
+
+export default defineComponent({
+  name: "Header",
+  data: function () {
+    return {
+      avatar: avatar,
+    };
+  },
+  methods: {
+    navigateHome: function () {
+      this.$router.push({ path: "/" });
+    },
+  },
+});
+</script>
--- src/views/BuurtBox.vue
+++ src/views/BuurtBox.vue
@@ -1,15 +1,18 @@
 <template>
+    <Header></Header>
     <UnderConstruction />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import UnderConstruction from "@/components/errors/UnderConstruction.vue"
+import Header from "@/components/Header.vue";
 
 export default defineComponent({
     name: "BuurtBox",
     components: {
-        UnderConstruction
+        UnderConstruction,
+        Header
     }
 })
 </script>
